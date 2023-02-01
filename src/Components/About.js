73893393618357ea08397a77bcca447d586efd33
Devesh Patel <devesh1260@gmail.com>
2023-02-01 20:18:13 +0530

    Context API

---
+++ src/Components/About.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import noteContext from '../Context/Notes/noteContext'
+import { useEffect } from 'react'
+
+export const About = () => {
+  const a = useContext(noteContext)
+
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line
+  }, [])
+  
+
+  return (
+    <div>This is about {a.state.name} in {a.state.college} </div>
+  )
+}
